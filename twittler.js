6f54dbc48db6476d413675f49bd75514fc898cf3
Andy Dam <andy@andydam.com>
2017-10-27 13:11:36 -0700

    added css and functions to load twitts

--- twittler.js
+++ twittler.js
@@ -2,18 +2,43 @@ $(document).ready(function(){
     $('#refresh').on('click', function(event) {
       event.stopPropagation();
       event.preventDefault();
-      loadMoreTwitts();
+      loadTwitts(false);
     });
 });
 
 function loadMoreTwitts() {
-  var $body = $('body');
   var index = streams.home.length - 1;
   while(index >= 0){
     var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    $tweet.appendTo($body);
+    displayTwitt(formatTwitt(tweet));
     index -= 1;
+  }
+}
+
+function formatTwitt(twitt) {
+  var formattedTwitt = '<li>';
+  formattedTwitt += '<span class=\"handle\">@' + twitt.user + '</span>';
+  formattedTwitt += ': ';
+  formattedTwitt += '<span class=\"content\">' + twitt.message + '</span>';
+  formattedTwitt += ' at ';
+  formattedTwitt += '<span class=\"time\">' + twitt.created_at + '</span>';
+  formattedTwitt += '</li>';
+  return formattedTwitt;
+}
+
+function displayTwitt(twittStr) {
+  var $body = $('#feed').find('.twitts');
+  var $twitt = $(twittStr);
+  $twitt.appendTo($body);
+}
+
+function loadTwitts(user, username) {
+  if (!user) {
+    var index = streams.home.length - 1;
+    while(index >= 0){
+      var tweet = streams.home[index];
+      displayTwitt(formatTwitt(tweet));
+      index -= 1;
+    }
   }
 }
