2325bc5aca9d6ec5e41fce1798c05496d4f9c726
Andy Dam <andy@andydam.com>
2017-10-27 14:22:00 -0700

    made improvements to profile viewing

--- twittler.js
+++ twittler.js
@@ -1,23 +1,54 @@
+var sessionData = {
+  'profile' : {
+    'name' : false,
+    'lastIndex' : 0
+  },
+  'stream' : {
+    'lastIndex' : 0
+  }
+};
+
 $(document).ready(function(){
-    $('#refresh').on('click', function(event) {
-      event.stopPropagation();
-      event.preventDefault();
-      loadTwitts(false);
-    });
+  $('#feed').on('click', '.refresh',function(event) {
+    //event.stopPropagation();
+    event.preventDefault();
+    if (!sessionData.profile.name) {
+      $('#twitter').hide();
+      loadTwitts(sessionData.profile.name);
+    } else {
+      loadTwitts(true, sessionData.profile.name);
+    }
+  });
+  $('#feed').on('click', '.profile', function(event) {
+    //if (!sessionData.profile) {
+      //sessionData.stream.lastIndex = 0;
+      sessionData.profile.lastIndex = 0;
+      sessionData.profile.name = $(this).data('username');
+      $('#feed').find('.twitts').text('');
+      $('#twitter').text(sessionData.profile.name + '\'s twitts');
+      $('#twitter').show();
+      loadTwitts(true, sessionData.profile.name);
+    //}
+  });
 });
 
-function loadMoreTwitts() {
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    displayTwitt(formatTwitt(tweet));
-    index -= 1;
+function loadTwitts(user, username) {
+  if (!user) {
+    for (var i = sessionData.stream.lastIndex; i < streams.home.length; i++) {
+      displayTwitt(formatTwitt(streams.home[i]));
+    }
+    sessionData.stream.lastIndex = streams.home.length;
+  } else {
+    for (var i = sessionData.profile.lastIndex; i < streams.users[username].length; i++) {
+      displayTwitt(formatTwitt(streams.users[username][i]));
+    }
+    sessionData.profile.lastIndex = streams.users[username].length;
   }
 }
 
 function formatTwitt(twitt) {
   var formattedTwitt = '<li>';
-  formattedTwitt += '<span class=\"handle\">@' + twitt.user + '</span>';
+  formattedTwitt += '<span class=\"handle\"><a href=\"#\" class=\"profile\" data-username=\"' + twitt.user + '\">@' + twitt.user + '</a></span>';
   formattedTwitt += ': ';
   formattedTwitt += '<span class=\"content\">' + twitt.message + '</span>';
   formattedTwitt += ' at ';
@@ -29,16 +60,5 @@ function formatTwitt(twitt) {
 function displayTwitt(twittStr) {
   var $body = $('#feed').find('.twitts');
   var $twitt = $(twittStr);
-  $twitt.appendTo($body);
-}
-
-function loadTwitts(user, username) {
-  if (!user) {
-    var index = streams.home.length - 1;
-    while(index >= 0){
-      var tweet = streams.home[index];
-      displayTwitt(formatTwitt(tweet));
-      index -= 1;
-    }
-  }
+  $twitt.prependTo($body);
 }
